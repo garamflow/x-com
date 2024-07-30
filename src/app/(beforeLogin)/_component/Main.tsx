7ec6f4b704c43a73cdb100ea8b6604e2deb6bff1
garamflow <garamflow@gmail.com>
2024-07-31 01:25:29 +0900

    fix: 로그인 시 연결 주소 수정

--- src/app/(beforeLogin)/_component/Main.tsx
+++ src/app/(beforeLogin)/_component/Main.tsx
@@ -23,7 +23,7 @@ export default function Main() {
 				</Link>
 				<h3>이미 트위터에 가입하셨나요?</h3>
 				<Link
-					href='/login'
+					href='/i/flow/login'
 					className={styles.login}
 				>
 					로그인
